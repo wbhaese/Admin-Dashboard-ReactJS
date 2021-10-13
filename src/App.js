fa4b651383e8bbb000aee8f74ff7202b3bf8f024
unknown <wbhaese@gmail.com>
2021-10-13 14:26:59 -0300

    Save progress till finish sidebar and navbar CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
-import './App.css';
+
+import Navbar from './components/navbar/Navbar';
 import Sidebar from './components/sidebar/Sidebar';
+
+import './App.css';
 
 const App = () => {
 
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const openSidebar = () => {
-    sidebarOpen(true);
+    setSidebarOpen(true);
   };
 
   const closeSidebar = () => {
@@ -15,6 +18,7 @@ const App = () => {
 
   return (
     <div className="container">
+      <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar}/>
       <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar}/>
     </div>
   );
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,37 @@
+import avatar from '../../assets/avatar.svg';
+
+import './Navbar.css';
+
+const Navbar = ({ sidebarOpen, openSidebar }) => {
+    return (
+        <nav className="navbar">
+            <div className="nav_icon" onClick={() => openSidebar()}>
+                <i className="fa fa-bars" aria-hidden="true"></i>
+            </div>
+
+            <div className="navbar__left">
+                <a href="#">Produtos</a>
+                <a href="#">Usuários</a>
+                <a href="#" className="active__link">Admin</a>
+            </div>
+            
+            <div className="navbar__right">
+                <a href="#">
+                    <i className="fa fa-search"></i>
+                </a>
+
+                <a href="#">
+                    <i className="fa fa-clock"></i>
+                </a>
+
+                <a href="#">
+                    <img width="30" src={avatar} alt="avatar"/>
+                </a>
+            </div>
+
+
+        </nav>
+    )
+}
+
+export default Navbar;
